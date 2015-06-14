91a0ad60cdf6f6b04700cd7b60dc147d28d97844
majkelos01 <mnz@gazeta.pl>
2015-06-14 20:42:04 +0800

    Update build_server.sh

--- build_server.sh
+++ build_server.sh
@@ -29,5 +29,7 @@ configure_memcached
 
 #install_firewall
 start_servers
+install_phpmyadmin
+start_servers
 
 echo -e "\033[32m Your server is set up and ready to start adding Wordpress sites. Just run 'bash add_site.sh' to add 1 or more sites. \033[0m"
